469cc17e6104bf92eb1996670f010cc999506dc4
glitteryash <glitteryash@gmail.com>
2023-10-29 18:32:58 +0900

    add button

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ add.addEventListener("click", (e) => {
   let todoMonth = form.children[1].value;
   let todoDate = form.children[2].value;
 
-  //create an todo item
+  //create a todo item
   let todo = document.createElement("div");
   todo.classList.add("todo");
   let text = document.createElement("p");
@@ -21,6 +21,24 @@ add.addEventListener("click", (e) => {
   time.innerText = todoMonth + "/" + todoDate;
   todo.appendChild(text);
   todo.appendChild(time);
+
+  //create green check and red trash can
+  let completeButton = document.createElement("button");
+  completeButton.classList.add("complete");
+  completeButton.innerHTML = '<i class="fa-regular fa-circle-check";"></i>';
+  completeButton.addEventListener("click", (e) => {
+    console.log(e.target.parentElement);
+    let todoItem = e.target.parentElement;
+    todoItem.classList.toggle("done");
+  });
+
+  let trashButton = document.createElement("button");
+  trashButton.classList.add("trash");
+  trashButton.innerHTML = '<i class="fa-solid fa-trash";"></i>';
+
+  todo.appendChild(completeButton);
+  todo.appendChild(trashButton);
+  todo.style.animation = "scaleUp .3s forwards";
 
   section.appendChild(todo);
 });
